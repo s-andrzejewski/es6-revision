6dd0a16e8580048f9607a7b5e73017d5d73c51ca
s-andrzejewski <s.andrzejewski.7@gmail.com>
2023-01-30 10:03:06 +0100

    First commit

---
+++ 1-block-scope.js
@@ -0,0 +1,28 @@
+// let cannot be redeclared - super feature (throws an error, which says that it's been already declared)
+// mostly I will be using const
+
+// functional scoping
+
+
+// but theres also block scoping:
+    const list = [1,2,3,4,5];
+
+    for(let i = 0; i < list.length; i++) {
+        console.log(i);
+    }
+
+    /* 
+    console.log(i); 
+    will give us an error, because i is only declared within the for {} scope
+    */
+
+    console.log("------------------------------")
+
+    // every where you have curly brackets - there you have an another (new) scope
+
+    // but only for let, because it's for declaring variable only in scope
+    // var is for global variable declaring:
+    for(var j = 0; j < list.length; j++) {
+        console.log(j);
+    }
+    console.log("and the j variable outside the scope is:", j);
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Javascript ES6 revision
+
+My ES6 revision, from ZeroToMastery course knowledge, to master it and use at my job.
+Each file includes also my personal notes.
+
+## Usage and contributing
+
+This package is made only for my education purposes, if you want to clone it please go ahead, but do not contribute inside of my repository.
